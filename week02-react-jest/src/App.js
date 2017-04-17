5176bbf8c21b5e4b70e8d0c9cec5ef2a81aabca5
jeff at school <tronsr71@yahoo.com>
2017-04-17 01:48:27 -0700

    Cleaning up code in App.js and App.test.js

--- week02-react-jest/src/App.js
+++ week02-react-jest/src/App.js
@@ -3,16 +3,31 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super(); // generic call for constructor
+    this.state = {
+        nine: '0'
+    }
+
+  }
+
+  bar = () => {
+    //alert("bar was called");
+    this.setState({ nine: '9' });
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to Hell</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Nine: {this.state.nine}
         </p>
+
+          <button className="elf" onClick = {this.bar}>Get Nine</button>
       </div>
     );
   }
--- week02-react-jest/src/App.test.js
+++ week02-react-jest/src/App.test.js
@@ -1,8 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { shallow } from 'enzyme';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+
+
+describe('React Jest Suite', function() {
+
+    it('renders our App component without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<App />, div);
+    });
+
+    it('renders and reads H1 text', () => {
+        const wrapper = shallow(<App />);
+        const welcome = <h2>Welcome to Hell</h2>;
+        expect(wrapper.contains(welcome)).toEqual(true);
+    });
+
+    it('renders button click message', () => {
+        const wrapper = shallow(<App />);
+        const nineSign = <p>Nine: 9</p>;
+        wrapper.find('button.elf').simulate('click');
+        expect(wrapper.contains(nineSign)).toEqual(true);
+    });
+
+
 });
+
