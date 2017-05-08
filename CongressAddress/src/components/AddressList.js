99d9b6c0b684180dd2185fa4f413a1220d74f386
jeff at school <tronsr71@yahoo.com>
2017-05-08 07:58:37 -0700

    inline with tag v5.0.3

--- CongressAddress/src/components/AddressList.js
+++ CongressAddress/src/components/AddressList.js
@@ -8,10 +8,11 @@ const addresses = [
     state: unknown,
     zip: unknown,
     phone: unknown,
-    website: unknown
+    website: unknown,
+    email: unknown,
+    contact: unknown
 
   },
-
   {
     firstName: 'Patty',
     lastName: 'Murray',
@@ -20,9 +21,47 @@ const addresses = [
     state: 'WA',
     zip: '98174',
     phone: '(206) 553-5545',
-    website: 'www.murray.senate.gov'
+    website: 'www.murray.senate.gov',
+    email: 'pmurray@gmail.com',
+    contact: 'http://some-office.gov'
 
-  }
+  },
+  {
+    firstName: "Lamar",
+    lastName: "Alexander",
+    street: "455 Dirksen Senate Office Building",
+    city: "Washington DC",
+    state: "TN",
+    zip: " 20510",
+    phone: "202-224-4944",
+    website: "https://www.alexander.senate.gov/public",
+    email: "",
+    contact: "http://www.alexander.senate.gov/public/index.cfm?p=Email"
+  },
+  {
+    firstName: "Roger",
+    lastName: "Wicker",
+    street: "555 Dirksen Senate Office Building",
+    city: "Washington DC",
+    state: "MS",
+    zip: " 20510",
+    phone: "202-224-6253",
+    website: "https://www.wicker.senate.gov",
+    email: "",
+    contact: "https://www.wicker.senate.gov/public/index.cfm/contact"
+  },
+  {
+    firstName: "Timothy",
+    lastName: "Kaine",
+    street: "231 Russell Senate Office Building",
+    city: "Washington DC",
+    state: "VA",
+    zip: " 20510",
+    phone: "202-224-4024",
+    website: "https://www.kaine.senate.gov",
+    email: "",
+    contact: "https://www.kaine.senate.gov/contact"
+  },
 
 ];
 
