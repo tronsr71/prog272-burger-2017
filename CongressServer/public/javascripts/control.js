399307d5c7ffd759fdaf0d1b4d3103b47ee11f86
jeff at school <tronsr71@yahoo.com>
2017-06-05 19:45:24 -0700

    Tag v10.0.1 (previous tag 10.0.0) didn't capture everything

--- CongressServer/public/javascripts/control.js
+++ CongressServer/public/javascripts/control.js
@@ -2,8 +2,8 @@ $(document).ready(function() { 'use strict';
   var insertUrl = '/insertValidCollection';
 
   function insertCollection() {
-    $.get(insertUrl, function(result) {
-      $('#display').html(JSON.stringify(result));
+    var jqxhr = $.get(insertUrl, function(result) {
+      alert( "success" );
       console.log(JSON.stringify(result, null, 4));
     })
       .done(function() {
@@ -19,9 +19,59 @@ $(document).ready(function() { 'use strict';
 
   $('#insertValidData').click(insertCollection);
 
-  $("#getAll").click(function() {
+  function getAll() {
     $.getJSON('/all-data', function(result) {
+      collection = result.allData;
       $('#display').html(JSON.stringify(result, null, 4));
     })
-  });
+      .done(function() {
+        console.log('second success');
+      })
+      .fail(function(error) {
+        alert(JSON.stringify(error.responseJSON, null, 4));
+      })
+      .always(function() {
+        console.log('finished');
+      });
+  }
+
 });
+
+
+
+
+
+// $(document).ready(function() { 'use strict';
+//   var insertUrl = '/insertValidCollection';
+//
+//   function insertCollection() {
+//     var jqxhr = $.get(insertUrl, function(result) {
+//       alert( "success" );
+//       console.log(JSON.stringify(result, null, 4));
+//     })
+//       .done(function() {
+//         console.log( "second success" );
+//       })
+//       .fail(function() {
+//         alert( "error" );
+//       })
+//       .always(function() {
+//         console.log( "finished" );
+//       });
+//   }
+//
+//   $('#insertValidData').click(insertCollection);
+//
+//   $("#getAll").click(function() {
+//     $.getJSON('/all-data', function(result) {
+//       router.get('/bar', function(request, response) {
+//         response.status(200).send({result: 'bar'});
+//       });
+//       $('#display').html(JSON.stringify(result, null, 4));
+//       router.get('/bar', function(request, response) {
+//         response.status(200).send({result: 'bar'});
+//       });
+//     });
+//   });
+// });
+//
--- CongressServer/routes/index.js
+++ CongressServer/routes/index.js
@@ -1,25 +1,60 @@
 var express = require('express');
 var router = express.Router();
-
-// var allMongo = require('./all-mongo');
-// var connect = require('./connect');
+//var politicians = require('../models/politicians');
+var allMongo = require('./all-mongo');
+var connect = require('./connect');
+// var connrouter.get('/bar', function(request, response) {
+//   response.status(200).send({result: 'bar'});
+// });
+// ect = require('./connect');
 
 /* GET home page. */
-router.get('/', function(req, res, next) { 'use strict';
-  res.render('index', {title: 'CongressServer-Burger'});
+router.get('/', function(req, res, next) {
+  'use strict';
+  res.render('index', {title: 'CongressServer'});
 });
 
-router.get('/bar', function(request, response) {
-  response.status(200).send({result: 'bar'});
+function checkConnection() {
+  if (!connect.connected) {
+    connect.doConnection('mlab');
+  }
+}
+
+router.get('/all-data', function(request, response) {
+  'use strict';
+  console.log('AllData route invoked.');
+  checkConnection();
+  allMongo.getAllData(response);
+});
+
+router.get('/emptyCollection', function(request, response) {
+  'use strict';
+  checkConnection();
+  allMongo.empty(response);
 });
 
 router.get('/insertValidCollection', function(request, response) {
-  response.status(200).send({result: 'insertValidCollection'});
+  'use strict';
+  console.log('Insert Valid Collection Called.');
+  //response.status(200).send({result: 'Insert valid Collection'});
+  checkConnection();
+  allMongo.readDataAndInsert(response);
+});
+
+/*
+router.get('/bar', function(request, response) {
+  response.status(200).send({result: 'bar'});
 });
 
 router.get('/bar/:id', function(request, response) {
   'use strict';
   response.status(200).send({param: request.params.id});
 });
+
+router.get('/:id', function(request, response) {
+  'use strict';
+  response.status(200).send({ result: request.params.id });
+});
+*/
 
 module.exports = router;
